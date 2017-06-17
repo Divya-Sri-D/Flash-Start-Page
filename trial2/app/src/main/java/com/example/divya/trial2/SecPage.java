d7114a58feb2e543f3880999bf8d08a6f584cfd3
Divya-Sri-D <divyasri1996@gmail.com>
2017-06-17 23:03:27 +0530

    added flash start page in a new project

---
+++ trial2/app/src/main/java/com/example/divya/trial2/SecPage.java
@@ -0,0 +1,13 @@
+package com.example.divya.trial2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sec_page);
+    }
+}
